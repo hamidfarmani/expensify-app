564bb4294094d1d83e9aca1bd9120674bcd303ed
Hamid Farmani <hamid.farmani@segula.se>
2022-04-29 10:35:11 +0200

    feat: add expense summary to dashboard page

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import numeral from "numeral";
+import selectExpenses from "../selectors/expenses";
+import selectExpensesTotal from "../selectors/expenses-total";
+
+const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? "expense" : "expenses";
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format("$0,0.00");
+  return (
+    <div>
+      <h1>
+        Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}
+      </h1>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  const visibileExpenses = selectExpenses(state.expenses, state.filters);
+
+  return {
+    expenseCount: visibileExpenses.length,
+    expensesTotal: selectExpensesTotal(visibileExpenses),
+  };
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
